b94ab1969a178078a83c61883a5066d9d36ef252
Sameer Ojha <146799637+Sameer172003@users.noreply.github.com>
2025-07-16 12:05:07 +0530

    added

--- tut4.py
+++ tut4.py
@@ -3,7 +3,19 @@
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+import sklearn as sk
 
 dataset=pd.read_csv("C:\\Users\\ojhas\\Dropbox\\PC\\Downloads\\test_data.csv")
+# print(dataset)
 
-print(dataset)
+# 2) Filling Numerical Missing Data
+
+print(dataset.select_dtypes(include="float64").columns)
+
+from sklearn.impute import SimpleImputer
+
+si=SimpleImputer(strategy="mean")
+ar=si.fit_transform(dataset[['Age', 'Salary']])
+
+print(pd.DataFrame(ar,columns=dataset.select_dtypes(include="float64").columns))
+
