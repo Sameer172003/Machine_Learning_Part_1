b83eceebbdcf930d8d3d360babf001e76c2ddb47
Sameer Ojha <146799637+Sameer172003@users.noreply.github.com>
2025-07-19 09:17:16 +0530

    added

--- tut13.py
+++ tut13.py
@@ -1,2 +1,12 @@
 # How to Handle Duplicate Data?
 
+import pandas as pd
+
+data={"name":["a","b","c","d","a","c"],"maths":[8,7,5,8,8,4],"hindi":[2,3,4,5,2,6]}
+
+df=pd.DataFrame(data)
+
+# df["duplicate"]=df.duplicated()
+
+# print(df)
+print(df.drop_duplicates())
